4e48f75152ceb38981b878b6ce89a972b089c9de
Alexis Yushin <alexis@apexive.com>
2024-04-01 13:47:35 -0400

    djangoflow/cloudfront-odoo/aws external module

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "cloudfront" {
   for_each        = var.ingress
   depends_on      = [cloudflare_record.a]
-  source          = "./modules/cloudfront-cloudflare"
+  source          = "djangoflow/cloudfront-odoo/aws"
   hostnames       = { (each.key) : each.value.domain }
   origin_hostname = cloudflare_record.a.hostname
 }
